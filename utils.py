f614ca634a41bfb72fec1265a6246b58a7ec5bb3
David Speck <david.speck@wooga.net>
2018-04-20 17:57:03 +0200

    Move convertion to utils.py

---
+++ utils.py
@@ -0,0 +1,20 @@
+import os
+import cv2
+
+def add(a, b):
+    return a + b
+
+def convert_video_to_frames(source_path, output_path, step_size):
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    vidcap = cv2.VideoCapture(source_path)
+    success, image = vidcap.read()
+    frame_index = 0
+    success = True
+    while success:
+        # save frame as JPEG file
+        cv2.imwrite("./data/poses/{1}/{0:d}_{1}.jpg".format(frame_index, output_path), image)
+        success, image = vidcap.read()
+        print('Read a new frame: {0} {1}'.format(frame_index, success))
+        frame_index += step_size
