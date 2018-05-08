428179ea783ae5f1771463d0ac6a6b7c418532e3
David Speck <david.speck@wooga.net>
2018-05-08 20:22:44 +0200

    Safe video timestamp with predicted poses

--- utils.py
+++ utils.py
@@ -60,6 +60,7 @@ def preprocess(video_name):
     print('pose model loaded')
 
     poses = []
+    times = []
     for i in range(frame_count):
         t = i * frame_length
         frame = video.get_frame(t)
@@ -73,26 +74,28 @@ def preprocess(video_name):
         # Extract maximum scoring location from the heatmap, assume 1 person
         pose = predict.argmax_pose_predict(scmap, locref, cfg.stride)
         poses.append(pose)
+        times.append(t)
 
         if i % 100 == 0:
             print(f'processed frame: {i}/{frame_count} elapsed time: {time.time() - start_time}')
 
     sess.close()
     print(f'saving poses at {csv_path}')
-    save_poses(np.array(poses), cfg, csv_path)
+    save_poses(np.array(poses), times, cfg, csv_path)
     print(f'saving audio at {audio_path}')
     video.audio.write_audiofile(audio_path)
     print(f'total time: {time.time() - start_time}')
 
-def save_poses(poses, cfg, output_path):
-    joints = {}
+def save_poses(poses, times, cfg, output_path):
+    dict = {}
+    dict['time'] = times
     for j in range(len(cfg.all_joints)):
         prefix = cfg.all_joints_names[j]
         for i in range(len(cfg.all_joints[j])):
-            joints[prefix + '_' + str(i) + '_x'] = poses[:,j,0]
-            joints[prefix + '_' + str(i) + '_y'] = poses[:,j,1]
-    df = pd.DataFrame(data = joints)
-    df.to_csv(output_path)
+            dict[prefix + '_' + str(i) + '_x'] = poses[:,j,0]
+            dict[prefix + '_' + str(i) + '_y'] = poses[:,j,1]
+    df = pd.DataFrame(data = dict)
+    df.to_csv(output_path, index=False)
 
 
 def convert_video_to_frames(source_path, output_path, step_size):
